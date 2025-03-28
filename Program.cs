09a69333cf52f99607233257b9415d74d094f0b4
winnicius-moura <winnicius.moura@gmail.com>
2025-03-28 19:26:27 -0300

    feat: add swagger

--- Program.cs
+++ Program.cs
@@ -1,27 +1,44 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
   options.UseSqlite("Data Source=app.db;Cache=Shared"));
+
+// Configure Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new OpenApiInfo
+  {
+    Title = "CRUD Studies API",
+    Version = "v1",
+    Description = "API documentation for the CRUD Studies project",
+    Contact = new OpenApiContact
+    {
+      Name = "Winnícius ",
+      Email = "winnicius.moura@gmail.com",
+      Url = new Uri("https://winnicius.pro")
+    }
+  });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.MapOpenApi();
+  app.UseSwagger();
+  app.UseSwaggerUI(c =>
+  {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CRUD Studies API v1");
+    c.RoutePrefix = string.Empty; 
+  });
 }
 
-app.MapControllers();
 app.UseRouting();
-
-app.MapControllerRoute(
-  name: "default",
-  pattern: "{controller=Home}/{action=Index}/{id?}"
-);
+app.MapControllers();
 
 app.Run();
